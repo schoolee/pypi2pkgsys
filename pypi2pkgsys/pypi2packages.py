e1bb7413f44da5ae48eb46e0c7dfb00e498dbae8
charlesw123456 <charlesw123456@00626f35-d055-0410-a824-970449c1b82d>
2008-09-10 14:33:19 +0000

    * Refine it.

--- pypi2pkgsys/pypi2packages.py
+++ pypi2pkgsys/pypi2packages.py
@@ -1,42 +1,23 @@
-# Copyright (C) 2008, Charles Wang <charlesw1234@163.com>
-# Author: Charles Wang <charlesw1234@163.com>
+# Copyright (C) 2008, Charles Wang <charlesw123456@gmail.com>
+# Author: Charles Wang <charlesw123456@gmail.com>
 
 import os
 import os.path
-import popen2
-import shutil
-import string
 import sys
 
-from ConfigParser import ConfigParser
-from distutils.errors import DistutilsError
 from pkg_resources import parse_requirements
 from setuptools.package_index import PackageIndex
 
+from pypi2pkgsys import patchdir, config
 from pypi2pkgsys.utils import *
-
-patchdir = os.path.join(os.path.dirname(__file__), 'patches')
-config = ConfigParser()
-config.read(os.path.join(patchdir, 'index.ini'))
-
-masked = {}
-broken_file = file(os.path.join(patchdir, 'broken.txt'))
-ln = broken_file.readline()
-while ln:
-    name, value = ln.split('=', 1)
-    name = name.strip(); value = value.strip()
-    masked[name] = value
-    ln = broken_file.readline()
-
-# If setup.py write something else by itself, the following code might not
-# work. FIX ME.
-popen_fmt = '(cd %s; python setup.py dump)'
+from pypi2pkgsys.pypi_utils import *
 
 class PYPI2Package(object):
     def __init__(self, PackageSystem, argv):
         self.options = { '--url' : 'http://pypi.python.org/simple',
                          '--download-dir' : '/var/tmp/pypi/downloads',
-                         '--unpack-dir' : '/var/tmp/pypi/unpack' }
+                         '--unpack-dir' : '/var/tmp/pypi/unpack',
+                         '--deps' : 'false' }
 
         self.pkgsys = PackageSystem()
         self.options = self.pkgsys.InitializeOptions(self.options)
@@ -57,9 +38,13 @@ class PYPI2Package(object):
         map(lambda diropt: ensure_dir(self.options[diropt]),
             ['--download-dir', '--unpack-dir'])
 
+        deps = self.options['--deps'].lower()
+        self.options['--deps'] = \
+            deps == 'true' or deps == 't' or \
+            deps == 'yes' or deps == 'y'
+
         self.options = self.pkgsys.FinalizeOptions(self.options)
 
-    def run(self):
         idx = 0
         while True:
             logfn = 'pypi-download.%03d.log' % idx
@@ -67,7 +52,13 @@ class PYPI2Package(object):
                 logfile = file(logfn, 'w')
                 break
             idx = idx + 1
+        self.options['log'] = logfile
 
+    def __del__(self):
+        self.options['log'].close()
+
+    def run(self):
+        logfp = self.options['log']
         # Prepare for iterations.
         pkgidx = PackageIndex(index_url = self.options['--url'])
         packages = self.packages
@@ -79,11 +70,15 @@ class PYPI2Package(object):
         while len(packages) > 0:
             new_packages = []
             for idx in xrange(len(packages)):
+                print
                 pkg = packages[idx]
 
                 pkgname = pkg.split('>=')[0].strip()
-                if pkgname in masked:
-                    print '%r is not accepted: %r' % (pkgname, masked[pkgname])
+
+                try: check_broken(pkgname)
+                except:
+                    exc_value = sys.exc_info()[1]
+                    print '%r is not accepted: %r' % (pkgname, exc_value)
                     continue
 
                 print 'Downloading %s ...' % pkg
@@ -92,25 +87,27 @@ class PYPI2Package(object):
                                    pkgidx.fetch_distribution(reqobj, dldir,
                                                              source = True),
                                parse_requirements([pkg]))[0]
-                except DistutilsError:
+                except:
                     exc_value = sys.exc_info()[1]
                     print 'Download %s failed: %s!' % (pkgname, exc_value)
-                    logfile.write('%s = %s\n' % (pkgname, exc_value))
-                    logfile.flush()
+                    logfp.write('%s = Download failed: %s\n' % \
+                                    (pkgname, exc_value))
+                    logfp.flush()
                     continue
                 if dist is None:
                     print 'Download %s failed!' % pkgname
-                    logfile.write('%s = No downloads.\n' % pkgname)
-                    logfile.flush()
+                    logfp.write('%s = No downloads.\n' % pkgname)
+                    logfp.flush()
                     continue
 
                 print 'Unpacking ...', pkg
-                ok_ornot, cfgmap = smart_archive(dist, unpackdir)
-                if not ok_ornot:
-                    print 'Unrecognized archive format: %s' % dist.location
-                    logfile.write("%s = Unrecognized archive format: %s\n" % \
-                                      (pkgname, dist.location))
-                    logfile.flush()
+                try: cfgmap = smart_archive(dist, unpackdir)
+                except:
+                    exc_value = sys.exc_info()[1]
+                    print 'Unpack %s failed: %s!' % (pkgname, exc_value)
+                    logfp.write('%s = Unpack failed: %s\n' % \
+                                    (pkgname, exc_value))
+                    logfp.flush()
                     continue
                 unpackpath = cfgmap['unpackpath']
 
@@ -131,66 +128,33 @@ class PYPI2Package(object):
                     print 'Applying %s' % p
                     os.system('(cd %s; patch -p0 < %s)' % \
                                   (unpackpath, os.path.join(patchdir, p)))
-                setup_path = os.path.join(unpackpath, 'setup.py')
-                if not os.path.isfile(setup_path):
-                    print '%s: No setup.py at all' % pkgname
-                    logfile.write('%s = No setup.py at all.\n' % pkgname)
-                    logfile.flush()
+                pkgtype = check_package_type(unpackpath)
+                if pkgtype == 'setup.py':
+                    fix_setup(unpackpath)
+                    print 'Get distribution args from %s' % unpackpath
+                    try: args = get_package_args(unpackpath)
+                    except:
+                        exc_value = sys.exc_info()[1]
+                        print 'Dump %s failed: %s.' % (pkgname, exc_value)
+                        logfp.write('%s = Dump args failed: %s.' % \
+                                        (pkgname, exc_value))
+                        logfp.flush()
+                        continue
+                else:
+                    print '%s: Unsupported package type.' % pkgname
+                    logfp.write('%s = Unsupport package type.' % pkgname)
+                    logfp.flush()
                     continue
-                self._setup_convert(setup_path)
 
-                print 'Get distribution args from %s' % unpackpath
-                p = popen2.popen2(popen_fmt % unpackpath)
-                p[1].close()
-                ln = p[0].readline()
-                while ln:
-                    if ln.strip() == '**** PyPI2PkgSys ****': break
-                    ln = p[0].readline()
-                if not ln: raise RuntimeError, 'dump does not work.'
-                c = p[0].readline()
-                p[0].close()
-                args = eval(c)
-                shutil.rmtree(unpackpath)
-                if 'install_requires' not in args or \
-                        args['install_requires'] is None:
-                    args['install_requires'] = []
-                if 'extras_require' not in args or \
-                        args['extras_require'] is None:
-                    args['extras_require'] = {}
+                args = fix_args(pkgname, args, pkg2license)
 
                 # Generate package from args and options.
                 updated, deps = \
                     self.pkgsys.GenPackage(args, self.options, cfgmap)
-                ok_packages.append(pkg)
                 new_packages = uniq_extend(new_packages, deps)
 
             # Process all required but not processed packages.
             packages = []
-            for pkg in new_packages:
-                if pkg not in ok_packages: packages.append(pkg)
-        logfile.close()
-
-    def _setup_convert(self, setup_path):
-        modified = False
-        setup_fp = file(setup_path)
-        output = ''
-        ln = setup_fp.readline()
-        while ln:
-            lnlist = ln.split()
-            if len(lnlist) >= 4 and lnlist[0] == 'from' and \
-                    lnlist[1] == 'distutils.core' and lnlist[2] == 'import' \
-                    and 'setup' in ln:
-                idx = ln.find('distutils.core')
-                ln = ln[:idx] + 'setuptools' + ln[idx + len('distutils.core'):]
-                modified = True
-            elif 'distutils.core.setup' in ln:
-                idx = ln.find('distutils.core')
-                ln = ln[:idx] + 'import setuptools; setuptools' + ln[idx + len('distutils.core'):]
-                modified = True
-            output = output + ln
-            ln = setup_fp.readline()
-        setup_fp.close()
-        if modified:
-            setup_fp = file(setup_path, 'w')
-            setup_fp.write(output)
-            setup_fp.close()
+            if self.options['--deps']:
+                for pkg in new_packages:
+                    if pkg not in ok_packages: packages.append(pkg)
