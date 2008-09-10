6aeb9bcaf2ff0dfca0db9be20bde5e964dcd2471
charlesw123456 <charlesw123456@00626f35-d055-0410-a824-970449c1b82d>
2008-09-10 14:35:24 +0000

    * Move package public values into __init__.py.

--- pypi2pkgsys/__init__.py
+++ pypi2pkgsys/__init__.py
@@ -1,1 +1,11 @@
-# Author: Charles Wang <charlesw1234@163.com>
+# Copyright (C) 2008, Charles Wang <charlesw123456@gmail.com>
+# Author: Charles Wang <charlesw123456@gmail.com>
+# License: BSD
+
+import os.path
+from ConfigParser import ConfigParser
+
+pkgroot = os.path.dirname(__file__)
+patchdir = os.path.join(pkgroot, 'patches')
+config = ConfigParser()
+config.read(os.path.join(patchdir, 'index.ini'))
