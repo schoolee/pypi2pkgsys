e02eded05da623d7c9ff67c1586bb70d4bd347c4
charlesw123456 <charlesw123456@00626f35-d055-0410-a824-970449c1b82d>
2008-09-14 08:26:16 +0000

    * Add new scripts to provide more features.

---
+++ scripts/pypi-manualcache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# Copyright (C) 2008, Charles Wang <charlesw123456@gmail.com>
+# Author: Charles Wang <charlesw123456@gmail.com>
+# License: BSD
+
+import sys
+from pkg_resources import Distribution
+from pypi2pkgsys.pypi_objects import pypicache
+
+if len(sys.argv) < 4:
+    print 'Usage: %s CACHE_ROOT CACHE_URL FILENAME,DISTNAME ...' % sys.argv[0]
+    sys.exit(-1)
+
+cacheroot = sys.argv[1]
+cacheurl = sys.argv[2]
+distlist = []
+for arg in sys.argv[3:]:
+    filename, distname = arg.split(',')
+    dist = Distribution.from_filename(filename, project_name = distname)
+    distlist.append(dist)
+cache = pypicache(cacheroot, cacheurl)
+cache.add_packages(distlist)
+del(cache)
