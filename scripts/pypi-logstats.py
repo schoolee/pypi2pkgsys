849666b23b2e6e35075c72a07b3f07bc6956fe5e
charlesw123456 <charlesw123456@00626f35-d055-0410-a824-970449c1b82d>
2008-09-14 09:53:48 +0000

    * Add manualvalue, so we can mask some packages whose ebuilds are written by hand.

--- scripts/pypi-logstats.py
+++ scripts/pypi-logstats.py
@@ -8,9 +8,11 @@ from pypi2pkgsys.pypi_objects import pypilog
 
 for log_path in sys.argv[1:]:
     logobj = pypilog(log_path)
-    ok, total = logobj.get_stats()
-    bad = total - ok
+    ok, manual, total = logobj.get_stats()
+    bad = total - ok - manual
     if total == 0: print '%s: empty.'
-    else: print '%s: %d(%5.2f%%) ok, %d(%5.2f%%) bad.' % \
-            (log_path, ok, ok * 100.0 / total, bad, bad * 100.0 / total)
+    else: print '%s: %d(%5.2f%%) ok, %d(%5.2f%%) manual, %d(%5.2f%%) bad.' % \
+            (log_path, ok, ok * 100.0 / total,
+             manual, manual * 100.0 / total,
+             bad, bad * 100.0 / total)
     del(logobj)
