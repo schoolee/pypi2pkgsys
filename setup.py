3704a7edec5f171a47324a91fe31ea48925caec3
charlesw123456 <charlesw123456@00626f35-d055-0410-a824-970449c1b82d>
2008-09-15 03:37:12 +0000

    * Update description.

--- setup.py
+++ setup.py
@@ -17,9 +17,16 @@ Now only ebuild for gentoo is generated. spec for rpm-base distribution,
 dpkg for dpkg-base distribution will be added in future.
 
 Many features are provided:
-* Wildcard is permitted in package names.
-* Cache is enabled by default so we shall not have to wait for the network.
-* Manage log automatically, it can record the fails so we can skip them.
+* Pre-defined, user-defined schemes for different usage.
+  * Update from PyPI and cached into localhost scheme is provided.
+  * Update from localhost scheme is provided, so the package file will not have to be downloaded again and again.
+* Wildcard is permitted in package names. For example: [Aa]*.
+* Full automatically log, all passed or failed packages(with reason) are recorded in log.
+* Some packages can be masked for manually written ebuild.
+* Multiple template support, to support different packages types. Now only standard distutils/setuptools template and single python template are provided. More template, such as webapp, will be added in future.
+* Package specific config, patches supported. User can manage their private config, patches in /etc/pypi2pkgsys too.
+* pypi-logstats.py is provided to show the statistics from log files.
+* pypi-manualcache.py is provided to generate localhost cache entries from self-downloaded package files. This reserve a great deal of time to download big files from pypi.python.org.
 
 Links:
 * News: http://code.google.com/p/pypi2pkgsys/wiki/News
