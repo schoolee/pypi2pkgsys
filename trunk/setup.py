6dd4256cd3679a5e781bcb20315ffbf5f5b5c2c4
charlesw123456 <charlesw123456@00626f35-d055-0410-a824-970449c1b82d>
2008-09-30 06:48:16 +0000

    * Do update scan and set version to 0.1.0.

--- trunk/setup.py
+++ trunk/setup.py
@@ -8,7 +8,7 @@ import glob
 import os.path
 from setuptools import setup, find_packages
 
-version = '0.0.7'
+version = '0.1.0'
 
 long_desc = \
     """Generate package files from PyPI index.
@@ -21,8 +21,9 @@ Many features are provided:
   * Update from PyPI and cached into localhost scheme is provided.
   * Update from localhost scheme is provided, so the package file will not have to be downloaded again and again.
 * Wildcard is permitted in package names. For example: [Aa]*.
-* Full automatically log, all passed or failed packages(with reason) are recorded in log.
-* Some packages can be masked for manually written ebuild.
+* Offline mirror support: Cache the downloaded packages and generate localhost cache entries index in the PyPI format. So we can treat our local-machine as a PyPI server.
+* Full automatically log, all passed or failed packages(with fail reason) are recorded in log.
+* Some packages can be masked for manually written ebuild/deb package.
 * Multiple template support, to support different packages types. Now only standard distutils/setuptools template and single python template are provided. More template, such as webapp, will be added in future.
 * Package specific config, patches supported. User can manage their private config, patches in /etc/pypi2pkgsys too.
 * pypi-logstats.py is provided to show the statistics from log files.
